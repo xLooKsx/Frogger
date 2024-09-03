c7175ec5fa005eef7a909632a03d34797ff376f0
xLooKsx <lu.oliveira.silva98@gmail.com>
2024-09-03 07:26:28 +0100

    feat: world and behavior creation  - creation of scene  - creation of palyer and enemy prefab  - enemy spawn using cooldown

---
+++ Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public GameObject carPrefab;
+    public bool isRightDirection;
+
+    public float cooldown;
+    public float currentCooldownTime;
+    public bool isTimeToGenerateNewCar;
+    void Start()
+    {
+        cooldown = 8f;
+        isTimeToGenerateNewCar = true;
+        currentCooldownTime = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InstatiateCars();
+        CalculateCoolDown();
+    }
+
+    private void CalculateCoolDown()
+    {
+        if(!isTimeToGenerateNewCar && currentCooldownTime >= cooldown){
+            isTimeToGenerateNewCar = true;
+        }else{
+            currentCooldownTime += Time.deltaTime;
+        }
+    }
+
+    private void InstatiateCars(){
+
+        if(isTimeToGenerateNewCar){
+
+            GameObject instantiatedObject = GameObject.Instantiate(carPrefab, this.transform.position, Quaternion.identity);
+            instantiatedObject.GetComponent<EnemyMovement>().isRightDirection = this.isRightDirection;
+
+            isTimeToGenerateNewCar = false;
+            currentCooldownTime = 0;
+        }
+
+    }
+}
